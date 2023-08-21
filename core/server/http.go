00908bfb8c5dcd389868d842ae9f51adb85ab64b
tonly <tonly@163.com>
2023-08-21 11:46:15 +0800

    fix: http.go

--- core/server/http.go
+++ core/server/http.go
@@ -23,8 +23,9 @@ func NewHttpServer(config *HttpServerConfig) *httpServer {
 		Http: http.Server{
 			Addr:           fmt.Sprintf(`%s:%d`, config.IP, config.Port),
 			Handler:        config.Handler,
-			ReadTimeout:    5 * time.Second,  //从链接被接受开始,到request body完全读取为止。
-			WriteTimeout:   10 * time.Second, //http:从request head读取结束开始到response write完成为止。
+			ReadTimeout:    5 * time.Second,  //从链接被接受开始,到request body完全读取为止.
+			WriteTimeout:   10 * time.Second, //http:从request head读取结束开始到response write完成为止.
+			IdleTimeout:    10 * time.Second, //空闲时长:如果IdleTimeout为0,则使用ReadTimeout.
 			MaxHeaderBytes: 1 << 20,
 		},
 	}
